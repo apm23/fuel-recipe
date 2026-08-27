49a4b32ddc3738378c66ac93c7965b4187620e19
apm23 <80390905+apm23@users.noreply.github.com>
2026-08-27 16:25:21 +0900

    Make invisible light nodes non-targetable

--- src/main/java/com/anjas/fuelrecipe/LightNodeBlock.java
+++ src/main/java/com/anjas/fuelrecipe/LightNodeBlock.java
@@ -1,9 +1,14 @@
 package com.anjas.fuelrecipe;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 final class LightNodeBlock extends Block {
     LightNodeBlock(BlockBehaviour.Properties properties) {
@@ -13,5 +18,10 @@ final class LightNodeBlock extends Block {
     @Override
     protected RenderShape getRenderShape(BlockState state) {
         return RenderShape.INVISIBLE;
+    }
+
+    @Override
+    protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return Shapes.empty();
     }
 }
